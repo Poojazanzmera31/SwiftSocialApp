18b5822c62d8aace19c82e9c10a01b005eb0b0b8
poojazanzmera <poojazanzmera31@gmail.com>
2025-11-25 12:29:18 +0530

    Initial Commit

---
+++ SwiftSocialApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftSocialApp
+//
+//  Created by POOJA ZANZMERA on 25/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftSocialApp/SwiftSocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSocialAppApp.swift
+//  SwiftSocialApp
+//
+//  Created by POOJA ZANZMERA on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
